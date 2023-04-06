38d0cb5de844966767be13c6cf8e22a45b3fd05d
Kyusans <xmelmacario@gmail.com>
2023-04-06 12:32:20 +0800

    added form validation

--- EnrollmentSystem.java
+++ EnrollmentSystem.java
@@ -6,73 +6,85 @@ public class EnrollmentSystem {
 	public static ArrayList<Student> students = new ArrayList<Student>();
 	public static ArrayList<Subject> subject = new ArrayList<Subject>();
 	public static String fullName = "";
+	public static int age;
 	public static int currentYear = Year.now().getValue();
 	public static int nextYear = currentYear + 1;
 	public static String schoolYear = String.format("%d-%d", currentYear, nextYear);
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		boolean exit = false;
-		boolean isValidLevel = false;
-		String level = "";
-		int sectionAEnrolled = 0;
-		int sectionBEnrolled = 0;
+		String level = "", firstName, lastName, middleName, email;
+		boolean validForm = false, exit = false, isValidLevel = false, validAge = false;
+		int sectionAEnrolled = 0, sectionBEnrolled = 0;
 
-		System.out.println("Welcome student!");
+		System.out.println("\n\nWelcome student!");
 		while (!exit) {
 			System.out.println("\n1. Enroll");
 			System.out.println("2. Exit");
 			System.out.print("Enter your choice: ");
-			int choice = scanner.nextInt();
-			scanner.nextLine();
+			String choice = scanner.nextLine();
 			switch(choice){
-				case 1:
+				case "1":
 					while(true){
-						System.out.print("\nEnter your first name: ");
-						String firstName = scanner.nextLine();
-						System.out.print("Enter your last name: ");
-						String lastName = scanner.nextLine();
-						System.out.print("Enter your middle name: ");
-						String middleName = scanner.nextLine();
-						System.out.print("Enter your email: ");
-						String email = scanner.nextLine();
-						System.out.print("Enter your age: ");
-						String age = scanner.nextLine();
+						do {
+							try {
+								System.out.print("Enter your age: ");
+								String ageStr = scanner.nextLine();
+								age = Integer.parseInt(ageStr);
+								validAge = true;
+							} catch (NumberFormatException e) {
+								System.out.println("\nPlease enter a valid age\n");
+							}
+						} while (!validAge);
+						do{
+							System.out.print("\nEnter your first name: ");
+							firstName = scanner.nextLine();
+							System.out.print("Enter your last name: ");
+							lastName = scanner.nextLine();
+							System.out.print("Enter your middle name: ");
+							middleName = scanner.nextLine();
+							System.out.print("Enter your email: ");
+							email = scanner.nextLine();
+							if(firstName.equals("") || lastName.equals("") || middleName.equals("") || email.equals("")){
+								System.out.println("\nThere was an empty input please try again.");
+							}else{
+								validForm = true;
+							}
+						}while(!validForm);
+
 						while(!isValidLevel){
 							System.out.print("What year do you want to enroll? \n"
-							+ "(1) 1st year\n"
-							+ "(2) 2nd year\n"
-							+ "(3) 3rd year\n"
-							+ "(4) 4th year\n"
+							+ "(1) Grade 1\n"
+							+ "(2) Grade 2\n"
+							+ "(3) Grade 3\n"
+							+ "(4) Grade 4\n"
+							+ "(5) Grade 5\n"
+							+ "(6) Grade 6\n"
 							+ "Enter your choice: ");
 							level = scanner.nextLine();
 							switch(level){
 								case "1":
-									subject.add(new Subject("Math", "IT100", "TBA", 3));
-									subject.add(new Subject("English", "IT110", "TBA", 3));
-									subject.add(new Subject("Science", "IT120", "TBA", 4));
-									subject.add(new Subject("History", "IT130", "TBA", 3));
+									addSubject(level);
 									isValidLevel = true;
 									break;
 								case "2":
-									subject.add(new Subject("Math", "MTH101", "TBA", 3));
-									subject.add(new Subject("English", "ENG101", "TBA", 3));
-									subject.add(new Subject("Science", "SCI101", "TBA", 4));
-									subject.add(new Subject("History", "HIS101", "TBA", 3));
+									addSubject(level);
 									isValidLevel = true;
 									break;
-								case "3":
-									subject.add(new Subject("Math", "MTH101", "TBA", 3));
-									subject.add(new Subject("English", "ENG101", "TBA", 3));
-									subject.add(new Subject("Science", "SCI101", "TBA", 4));
-									subject.add(new Subject("History", "HIS101", "TBA", 3));
+								case "3":	
+									addSubject(level);
 									isValidLevel = true;
 									break;
 								case "4":
-									subject.add(new Subject("Math", "MTH101", "TBA", 3));
-									subject.add(new Subject("English", "ENG101", "TBA", 3));
-									subject.add(new Subject("Science", "SCI101", "TBA", 4));
-									subject.add(new Subject("History", "HIS101", "TBA", 3));	
+									addSubject(level);
+									isValidLevel = true;
+									break;
+								case "5":
+									addSubject(level);
+									isValidLevel = true;
+									break;
+								case "6":
+									addSubject(level);
 									isValidLevel = true;
 									break;
 								default:
@@ -91,7 +103,7 @@ public class EnrollmentSystem {
 						+ "Full Name: " + fullName
 						+ "\nEmail: " + email
 						+ "\nAge: " + age
-						+ "\nYear level: " + level
+						+ "\nGrade level: " + level
 						+ "\n\nType Y to confirm or type any other key to make any changes: ");
 						String confirm = scanner.nextLine();
 
@@ -111,7 +123,7 @@ public class EnrollmentSystem {
 						}
 					}
 					break;
-				case 2:
+				case "2":
 					System.out.println("\nExiting program...");
 					System.exit(0);
 					break;
@@ -127,10 +139,21 @@ public class EnrollmentSystem {
 		System.out.println("\n\n\n\nStudent Name: " + fullName.toUpperCase()
 		+ "\nSection: " + section
 		+ "\nSchool Year: " + schoolYear + "\n"
+		+ "---------------------------------------------------"
 		);
-		System.out.printf("%-15s%-15s%-15s%-10s\n", "Subject", "Code", "Room", "Credit");
+		System.out.printf("%-20s%-20s%-20s\n", "Subject", "Code", "Credit");
 		for (Subject subjects : subject) {
-			System.out.printf("%-15s%-15s%-15s%-10d\n", subjects.getSubjectName(), subjects.getCode(), subjects.getRoom(),subjects.getCreditHours());
+			System.out.printf("%-20s%-20s%-20d\n", subjects.getSubjectName(), subjects.getCode(),subjects.getCreditHours());
 		}
 	}
+
+	public static void addSubject(String gradeLevel){
+		subject.add(new Subject("Math", "MTH10" + gradeLevel, 3));
+		subject.add(new Subject("English", "ENG10" + gradeLevel, 3));
+		subject.add(new Subject("Science", "SCI10" + gradeLevel, 4));
+		subject.add(new Subject("Social Studies", "SS10" + gradeLevel, 3));
+		subject.add(new Subject("MAPEH", "MAPEH10" + gradeLevel, 2));
+		subject.add(new Subject("TLE", "TLE10" + gradeLevel, 2));
+		subject.add(new Subject("Values Education", "VAL10" + gradeLevel, 2));
+	} 
 }
--- Subject.java
+++ Subject.java
@@ -1,13 +1,11 @@
 public class Subject{
   private String subjectName;
   private String code;
-  private String room;
   private int creditHours;
 
-  public Subject(String subjectName, String code, String room, int creditHours){
+  public Subject(String subjectName, String code, int creditHours){
     this.subjectName = subjectName;
     this.code = code;
-    this.room = room;
     this.creditHours = creditHours;
   }
   
@@ -17,10 +15,6 @@ public class Subject{
   
   public String getCode(){
     return code;
-  }
-
-  public String getRoom(){
-    return room;
   }
 
   public int getCreditHours(){
